76d77411d309eafbc31f76c0fe06ede164721bf6
Evgenii Egorov <e.egorov@iva.ru>
2024-03-11 14:26:38 +0300

    Add token

--- import/effectsList.js
+++ import/effectsList.js
@@ -1,4 +1,7 @@
 export const importedEffectsList = [
+  'ActionunitsGrout.zip',
+  'blur_bg.zip',
+  'BulldogHarlamov.zip'
   // Place your effect zip-file to import folder and add here zip-file name. Example:
   //'your_effect_1.zip',
   // 'your_effect_2.zip'
